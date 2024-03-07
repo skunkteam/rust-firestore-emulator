cabd8ff992204063625f7eb1fbba3f705507c889
Paco van der Linden <pavadeli@gmail.com>
2024-03-07 12:49:02 +0100

    add hybrid_axum_tonic for hybrid REST and GRPC endpoints on same host:port

---
+++ crates/hybrid_axum_tonic/tests/lib.rs
@@ -0,0 +1,87 @@
+mod common;
+
+use std::{sync::Mutex, time::Duration};
+
+use axum::{
+    middleware::{from_fn, Next},
+    response::Response,
+    routing::get,
+    Router,
+};
+use common::{
+    proto::{
+        test1_client::Test1Client, test1_server::Test1Server, test2_client::Test2Client,
+        test2_server::Test2Server, Test1Request, Test2Request,
+    },
+    server::{Test1Service, Test2Service},
+};
+use hybrid_axum_tonic::{GrpcStatus, NestTonic, RestGrpcService};
+use hyper::Request;
+use tonic::transport::Channel;
+
+async fn do_nothing<B>(req: Request<B>, next: Next<B>) -> Result<Response, GrpcStatus> {
+    Ok(next.run(req).await)
+}
+
+async fn cancel_request<B>(_req: Request<B>, _next: Next<B>) -> Result<Response, GrpcStatus> {
+    Err(tonic::Status::cancelled("Canceled").into())
+}
+
+#[tokio::test]
+async fn test_hybrid() {
+    tokio::task::spawn(async move {
+        let grpc_router1 = Router::new()
+            .nest_tonic(Test1Server::new(Test1Service {
+                state: Mutex::new(10),
+                str:   String::new(),
+            }))
+            .layer(from_fn(do_nothing));
+
+        let grpc_router2 = Router::new()
+            .nest_tonic(Test2Server::new(Test2Service))
+            .layer(from_fn(cancel_request));
+
+        let grpc_router = grpc_router1.merge(grpc_router2);
+
+        let rest_router =
+            Router::new().nest("/", Router::new().route("/123", get(|| async move {})));
+
+        let service = RestGrpcService::new(rest_router, grpc_router);
+
+        axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
+            .serve(service.into_make_service())
+            .await
+            .unwrap();
+    });
+
+    tokio::time::sleep(Duration::from_millis(100)).await;
+
+    let channel = Channel::from_static("http://127.0.0.1:8080")
+        .connect()
+        .await
+        .unwrap();
+
+    let mut client1 = Test1Client::new(channel.clone());
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+
+    let channel = Channel::from_static("http://127.0.0.1:8080")
+        .connect()
+        .await
+        .unwrap();
+
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+    client1.test1(Test1Request {}).await.unwrap();
+
+    let mut client2 = Test2Client::new(channel);
+    assert_eq!(
+        client2.test2(Test2Request {}).await.unwrap_err().code(),
+        tonic::Code::Cancelled,
+    );
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 
+use axum::{routing::get, Router};
 use emulator::FirestoreEmulator;
 use googleapis::google::firestore::v1::firestore_server::FirestoreServer;
-use tonic::{codec::CompressionEncoding, transport::Server};
+use hybrid_axum_tonic::{NestTonic, RestGrpcService};
+use tonic::codec::CompressionEncoding;
 use tracing::info;
 
 mod database;
@@ -14,13 +16,18 @@ const MAX_MESSAGE_SIZE: usize = 50 * 1024 * 1024;
 
 #[tokio::main]
 pub async fn run(host_port: SocketAddr) -> color_eyre::Result<()> {
-    let emulator = FirestoreEmulator::new();
-    let firestore = FirestoreServer::new(emulator)
+    let emulator = Arc::new(FirestoreEmulator::new());
+    let firestore = FirestoreServer::from_arc(emulator)
         .accept_compressed(CompressionEncoding::Gzip)
         .send_compressed(CompressionEncoding::Gzip)
         .max_decoding_message_size(MAX_MESSAGE_SIZE);
 
-    let server = Server::builder().add_service(firestore).serve(host_port);
+    let grpc_router = Router::new().nest_tonic(firestore);
+    let rest_router = Router::new().route("/", get(|| async { "Hello world!" }));
+
+    let service = RestGrpcService::new(rest_router, grpc_router);
+
+    let server = axum::Server::bind(&host_port).serve(service.into_make_service());
 
     info!("Firestore listening on {}", host_port);
 
