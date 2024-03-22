a3bd69e0763a67b89247808e548f12788a535459
Paco van der Linden <pavadeli@gmail.com>
2024-03-22 09:53:13 +0100

    fix: Increase read and write lock times and reduce timeouts in tests for more stable unit-tests

--- crates/firestore-database/src/config.rs
+++ crates/firestore-database/src/config.rs
@@ -3,7 +3,7 @@ pub struct FirestoreConfig {
     /// Enable more accurate lock timeouts.
     ///
     /// In Cloud Firestore, transactions can take up to 15 seconds before aborting because of
-    /// contention. By default, in the emulator, this is reduced to 1 second for faster unit-tests.
-    /// Enable this feature to simulate the Cloud Firestore more accurately.
+    /// contention. By default, in the emulator, this is reduced to 2 seconds for faster
+    /// unit-tests. Enable this feature to simulate the Cloud Firestore more accurately.
     pub long_contention_timeout: bool,
 }
