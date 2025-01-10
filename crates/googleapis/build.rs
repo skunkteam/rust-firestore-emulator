c844c16d8fbe9cc0e3a6c57efcefb5cfa3aae21f
Paco van der Linden <pavadeli@gmail.com>
2025-01-10 14:20:21 +0100

    show bytes_value (byte slices) as base64 in json

--- crates/googleapis/build.rs
+++ crates/googleapis/build.rs
@@ -3,26 +3,30 @@ fn main() {
         .build_client(false)
         .include_file("googleapis.rs")
         .compile_well_known_types(true)
-        .message_attribute(".google.type.LatLng", "#[derive(serde::Serialize, Copy)]")
-        .message_attribute(
+        .bytes(["bytes_value"])
+        .type_attribute(".google.type.LatLng", "#[derive(serde::Serialize, Copy)]")
+        .type_attribute(
             ".google.protobuf.Timestamp",
             "#[derive(Eq, PartialOrd, Ord, serde::Serialize, Copy)]",
         )
-        .message_attribute(
+        .type_attribute(
             ".google.firestore.v1.Document",
             "#[derive(serde::Serialize)]",
         )
-        .message_attribute(".google.firestore.v1.Value", "#[derive(serde::Serialize)]")
-        .message_attribute(".google.firestore.v1.Value", "#[serde(transparent)]")
-        .enum_attribute(".google.firestore.v1.Value", "#[derive(serde::Serialize)]")
-        .enum_attribute(".google.firestore.v1.Value", "#[serde(untagged)]")
-        .message_attribute(
+        .type_attribute(
             ".google.firestore.v1.ArrayValue",
             "#[derive(serde::Serialize)]",
         )
-        .message_attribute(
+        .type_attribute(
             ".google.firestore.v1.MapValue",
             "#[derive(serde::Serialize)]",
+        )
+        .type_attribute(".google.firestore.v1.Value", "#[derive(serde::Serialize)]")
+        .message_attribute(".google.firestore.v1.Value", "#[serde(transparent)]")
+        .enum_attribute(".google.firestore.v1.Value", "#[serde(untagged)]")
+        .field_attribute(
+            "bytes_value",
+            r#"#[serde(serialize_with="crate::bytes_base64::serialize")]"#,
         )
         .compile(
             &["include/google/firestore/v1/firestore.proto"],
