f59de3a8a26a066ec2fe5c67738a7e53c70937df
Paco van der Linden <pavadeli@gmail.com>
2025-01-13 20:09:31 +0100

    chore: update deps (#103)

--- crates/googleapis/src/ser.rs
+++ crates/googleapis/src/ser.rs
@@ -11,18 +11,17 @@ pub(crate) fn as_null<T, S: Serializer>(_v: T, s: S) -> Result<S::Ok, S::Error> 
 
 #[cfg(test)]
 mod tests {
-    use prost::bytes::Bytes;
-
-    use crate::google::firestore::v1::{value::ValueType, Value};
+    use crate::google::firestore::v1::Value;
 
     #[test]
     fn serialization() {
         let values = [
             Value::null(),
-            Value {
-                value_type: Some(ValueType::BytesValue(Bytes::from_static(b"\xff\xff\xbe"))),
-            },
+            Value::bytes(b"\x03\x07\xac\xa2\x67\xbf\x6d\xab\x1e\xeb\x6f\xbe".as_slice()),
         ];
-        assert_eq!(serde_json::to_string(&values).unwrap(), r#"[null,"//++"]"#);
+        assert_eq!(
+            serde_json::to_string(&values).unwrap(),
+            r#"[null,"Awesome/base62++"]"#
+        );
     }
 }
