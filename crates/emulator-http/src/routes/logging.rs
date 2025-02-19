134091250e9fff528aac9fcda517cf312c51516c
Paco van der Linden <pavadeli@gmail.com>
2025-02-19 13:41:18 +0100

    chore: update deps and version number (#120)
    * chore: update deps and version number
    
    * add test to make sure we didn't break dynamic logging

--- crates/emulator-http/src/routes/logging.rs
+++ crates/emulator-http/src/routes/logging.rs
@@ -74,3 +74,109 @@ async fn stop_capture<S: Tracing + 'static>(
         .ok_or_else(RestError::not_found)
         .map(|mut subscriber| subscriber.drain())
 }
+
+#[cfg(test)]
+mod tests {
+    use emulator_database::{FirestoreConfig, FirestoreProject};
+    use emulator_tracing::DefaultTracing;
+    use googletest::prelude::*;
+    use reqwest::Client;
+    use tokio::net::TcpListener;
+
+    use crate::RouterBuilder;
+
+    #[gtest]
+    #[tokio::test]
+    async fn test_dynamic_tracing() -> Result<()> {
+        let project = FirestoreProject::new(FirestoreConfig::default());
+        let project = Box::leak(Box::new(project));
+        let tracing = DefaultTracing::start();
+        let router = RouterBuilder::new(project)
+            .add_dynamic_tracing(tracing)
+            .build();
+
+        let tcp_listener = TcpListener::bind("127.0.0.1:0").await?;
+        let host = tcp_listener.local_addr()?;
+        tokio::spawn(async {
+            axum::serve(tcp_listener, router).await.unwrap();
+        });
+
+        let client = Client::new();
+        let handle_url = format!("http://{host}/emulator/v1/logging/my-handle");
+        let poll_url = format!("{handle_url}:poll");
+
+        // Start capturing logging of all tower_http DEBUG messages
+        let logs = client
+            .post(&handle_url)
+            .body("tower_http=debug")
+            .send()
+            .await?
+            .error_for_status()?
+            .text()
+            .await;
+
+        expect_that!(logs, ok(eq("")), "we start with no logging captured");
+
+        let logs = client
+            .post(&poll_url)
+            .send()
+            .await?
+            .error_for_status()?
+            .text()
+            .await;
+        expect_that!(
+            logs,
+            ok(all!(
+                contains_substring("DEBUG"),
+                contains_substring("tower_http::trace")
+            )),
+            "the first poll shows some DEBUG messages from the tower_http crate"
+        );
+
+        // Now we will adjust the logging directives of this capture handle...
+        let logs = client
+            .post(&handle_url)
+            .body("warn")
+            .send()
+            .await?
+            .error_for_status()?
+            .text()
+            .await;
+        expect_that!(
+            logs,
+            ok(all!(
+                contains_substring("DEBUG"),
+                contains_substring("tower_http::trace")
+            )),
+            "we still receive the logs that were captured up until now"
+        );
+
+        let logs = client
+            .post(&poll_url)
+            .send()
+            .await?
+            .error_for_status()?
+            .text()
+            .await;
+        expect_that!(
+            logs,
+            ok(eq("")),
+            "but this time there is no logging, because no WARN-level logs have been captured"
+        );
+
+        // Stop the capture
+        client
+            .delete(&handle_url)
+            .send()
+            .await?
+            .error_for_status()?;
+
+        let unknown_poll_status = client.post(&poll_url).send().await?.status();
+        expect_that!(
+            unknown_poll_status,
+            eq(404),
+            "polling on an unknown handle gives a 404"
+        );
+        Ok(())
+    }
+}
